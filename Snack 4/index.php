3a8765acc63e3b95d803ef1818e52c811995fdb6
UmbertoV <umberto.ottone@gmail.com>
2020-01-22 19:22:46 +0100

    Snack 4 FINITO

---
+++ Snack 4/index.php
@@ -0,0 +1,43 @@
+<?php
+
+$array_numbers = [];  
+
+// ciclo for
+for ($i = 0; count($array_numbers) < 15; $i++) {
+    $number = rand(1,100);
+    if(!in_array($number, $array_numbers)){
+        array_push($array_numbers, $number);
+    }
+}; 
+
+//ciclo while
+// $i = 0;
+// while (count($array_numbers) < 15) {
+//     $number = rand(1,15);
+//     if (!in_array($number, $array_numbers)) {
+//         array_push($array_numbers, $number);
+//     }
+//     $i++;
+// }; 
+
+
+// ciclo do while
+// $i = 0;
+// do {
+//     $number = rand(1,15);
+//     if (!in_array($number, $array_numbers)) {
+//         array_push($array_numbers, $number);
+//     }
+//     $i++;
+// } while (count($array_numbers) < 15);
+
+
+echo '<pre>';
+var_dump($array_numbers);
+echo '</pre>'; 
+
+
+
+
+
+?>
